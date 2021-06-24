1c29888b610dbd741d6c73ad8970513966d0ba89
Alex <olivesargreek@gmail.com>
2021-06-24 12:52:07 +0300

    modified: разбили компоненты проекта на отдельные папки = причесали проект по фен-шую

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+
+import AppHeader from "./components/app-header/app-header";
+import SearchPanel from "./components/search-panel/search-panel";
+import TodoList from "./components/todo-list/todo-list";
+import ItemStatusFilter from "./components/item-status-filter/item-status-filter";
+import  './index.css';
 
 const App = () => {
 
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Make Awesome App', important: true },
-        { label: 'Eat meat', important: false },
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Eat meat', important: false, id: 3 }
     ];
     // const isLoggedIn = true;
     // const loginBox = <span>Log in please</span>
     // const welcomeBox = <span>Welcome Back</span>
     return (
-        <div>
+        <div className="todo-app">
             {/*{ isLoggedIn ? welcomeBox : loginBox }*/}
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos={ todoData } />
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter/>
+            </div>
+            <TodoList todos= { todoData } />
         </div>
     );
 };
