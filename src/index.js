96dba93930d7041d4bb54826d8d20afd886d98a2
Alex <olivesargreek@gmail.com>
2021-06-24 17:57:43 +0300

    modidied: применили конструктор класса === главное работает todo-list-item

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from "./components/app-header/app-header";
-import SearchPanel from "./components/search-panel/search-panel";
-import TodoList from "./components/todo-list/todo-list";
-import ItemStatusFilter from "./components/item-status-filter/item-status-filter";
-import  './index.css';
+import App from './components/app';
 
-const App = () => {
-
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Eat meat', important: false, id: 3 }
-    ];
-    // const isLoggedIn = true;
-    // const loginBox = <span>Log in please</span>
-    // const welcomeBox = <span>Welcome Back</span>
-    return (
-        <div className="todo-app">
-            {/*{ isLoggedIn ? welcomeBox : loginBox }*/}
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter/>
-            </div>
-            <TodoList todos= { todoData } />
-        </div>
-    );
-};
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
