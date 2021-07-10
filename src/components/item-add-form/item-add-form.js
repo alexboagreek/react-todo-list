c855c723f39191724db98b6d2d74a8e0de7dc362
Alex <olivesargreek@gmail.com>
2021-07-10 15:09:38 +0300

    modified: сделана форма для добавления новой задачи в TodoList отменили действие формы при сабмите при помощи e.preventDafault

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-
+    state = {
+        label: ''
+    };
+onLabelChange = (e) => {
+ this.setState({
+     label: e.target.value
+ });
+};
+onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+};
 render () {
     return (
-        <div className="item-add-form">
+        <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+            <input type="text" className="form-control" onChange={ this.onLabelChange } placeholder="What needs to be done ?" />
             <button className="btn btn-outline-secondary"
-            onClick={() => this.props.onItemAdded }>Add New Item</button>
-        </div>
+                >Add New Item
+            </button>
+        </form>
     )
 }
 
