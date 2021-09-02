2015d72cf7202ac55d415958e34a9a6534508ed3
Alex <olivesargreek@gmail.com>
2021-09-02 14:59:33 +0300

    feat: реалтзована функция поиска в SearcPanel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,13 +9,16 @@ import  './app.css';
 
 export default class App extends Component {
     maxId = 100;
+
     state = {
         todoData : [
             this.createTodoItem('Drink Coffee'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Eat meat and protein'),
-        ]
+        ],
+        term: ''
     };
+
     createTodoItem(label) {
         return {
             label,
@@ -81,9 +84,23 @@ export default class App extends Component {
         });
     };
 
+    onSearchChange = (term) => {
+        this.setState( { term });
+    };
+
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter((item) => {
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
+    };
+
 
     render() {
-        const { todoData } = this.state;
+        const { todoData, term } = this.state;
+        const visibleItems = this.search(todoData, term);
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
         return (
@@ -91,11 +108,12 @@ export default class App extends Component {
                 {/*{ isLoggedIn ? welcomeBox : loginBox }*/}
                 <AppHeader toDo={ todoCount } done={ doneCount } />
                 <div className="top-panel d-flex">
-                    <SearchPanel />
+                    <SearchPanel 
+                    onSearchChange = { this.onSearchChange } />
                     <ItemStatusFilter/>
                 </div>
                 <TodoList
-                    todos = { todoData }
+                    todos = { visibleItems }
                     onDeleted = { this.deleteItem }
                     onToggleImportant = { this.onToggleImportant }
                     onToggleDone = { this.onToggleDone }
